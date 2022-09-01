c6a13aedbe9c159c220a460a4cb5ebe75976e5d0
sieunju <j.sieun73@gmail.com>
2022-09-01 19:13:22 +0900

    refactor: 네트워크 전용 모듈 생성 (분활작업)

--- android/core/src/main/java/com/hmju/core/data/qualifiers/TrackingInterceptor.kt
+++
@@ -1,7 +0,0 @@
-package com.hmju.core.data.qualifiers
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION)
-annotation class TrackingInterceptor
