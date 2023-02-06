45b327a48f8018c1896099daa573809f6fcde83a
sieunju <j.sieun73@gmail.com>
2023-02-07 00:38:36 +0900

    refactor: RxCallAdapter, CoroutinesCallAdapter 겸용 가능하도록 처리

--- android/network/src/main/java/com/hmju/network/adapter/CoroutineErrorHandlingCallAdapter.kt
+++ android/network/src/main/java/com/hmju/network/adapter/CoroutineErrorHandlingCallAdapter.kt
@@ -1,6 +1,6 @@
 package com.hmju.network.adapter
 
-import com.hmju.core.model.base.ApiResponse
+import com.hmju.core.model.base.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,76 +14,95 @@ import java.lang.reflect.Type
 
 /**
  * Description : Coroutines Call Adapter
- * @
+ * References RxJava3CallAdapterFactory
  * Created by juhongmin on 2023/02/04
  */
 class CoroutineErrorHandlingCallAdapter(
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 ) : CallAdapter.Factory() {
 
+    companion object {
+        fun create(): CallAdapter.Factory {
+            return CoroutineErrorHandlingCallAdapter()
+        }
+    }
+
     override fun get(
         returnType: Type,
         annotations: Array<out Annotation>,
         retrofit: Retrofit
-    ): CallAdapter<*, *> {
-        val callType = getParameterUpperBound(0, returnType as ParameterizedType)
-        val resultType = getParameterUpperBound(0, callType as ParameterizedType)
-        Timber.d("ResultType $resultType")
-        return CallExecuteCoroutineCallAdapter(resultType, coroutineScope)
-    }
+    ): CallAdapter<*, *>? {
+        return when (getRawType(returnType)) {
+            Call::class.java -> {
+                // ApiResponse<JSend...> -> Original (Call<ApiResponse<JSend...>)
+                val callType = getParameterUpperBound(0, returnType as ParameterizedType)
 
-    private class CallExecuteCoroutineCallAdapter(
-        private val responseType: Type,
-        private val coroutineScope: CoroutineScope
-    ) : CallAdapter<Any, Call<ApiResponse<Any>>> {
-        // Request, Response
-
-        override fun responseType() = responseType
-
-        override fun adapt(call: Call<Any>): Call<ApiResponse<Any>> {
-            return CallExecuteCoroutinesDelegate(call, coroutineScope)
-        }
-    }
-
-    private class CallExecuteCoroutinesDelegate(
-        private val originCall: Call<Any>,
-        private val coroutineScope: CoroutineScope
-    ) : Call<ApiResponse<Any>> {
-        override fun clone(): Call<ApiResponse<Any>> {
-            return CallExecuteCoroutinesDelegate(originCall.clone(), coroutineScope)
-        }
-
-        override fun enqueue(callback: Callback<ApiResponse<Any>>) {
-            coroutineScope.launch {
-                try {
-                    val response = originCall.awaitResponse()
-                    val body = response.body()
-                    if (body != null) {
-                        Timber.d("enqueue SUCCESS $body")
-                        callback.onResponse(
-                            this@CallExecuteCoroutinesDelegate,
-                            Response.success(ApiResponse.Success(body))
-                        )
-                    } else {
-                        Timber.d("enqueue FAIL $response")
-                        callback.onResponse(
-                            this@CallExecuteCoroutinesDelegate,
-                            Response.success(ApiResponse.Fail(response))
-                        )
-                    }
-
-                } catch (ex: Exception) {
-                    Timber.d("enqueue ERROR $ex")
-                    callback.onResponse(
-                        this@CallExecuteCoroutinesDelegate,
-                        Response.success(ApiResponse.Error(ex))
-                    )
+                // ApiResponse 으로 감싸져 있는지 확인
+                if (getRawType(callType) == ApiResponse::class.java) {
+                    // ApiResponse Find {<JSend...>}
+                    val jsendType = getParameterUpperBound(0, callType as ParameterizedType)
+                    Timber.d("ApiResponse Format $jsendType")
+                    CoroutineCallAdapterWrapper(jsendType, coroutineScope)
+                } else {
+                    null
                 }
+            }
+            else -> null
+        }
+    }
+
+    inner class CoroutineCallAdapterWrapper<R : Type>(
+        private val responseType: R,
+        private val coroutineScope: CoroutineScope
+    ) : CallAdapter<R, Any> {
+        // CallAdapter<Request,Response>
+
+        /**
+         * @see JSendObj
+         * @see JSendObjWithMeta
+         * @see JSendList
+         * @see JSendListWithMeta
+         * 이외는 isValid 체크 X
+         */
+        override fun responseType(): Type {
+            return responseType
+        }
+
+        override fun adapt(call: Call<R>): Any {
+            return CallEnqueueDelegate(call, coroutineScope)
+        }
+    }
+
+    inner class CallEnqueueDelegate<T>(
+        private val originCall: Call<T>,
+        private val coroutineScope: CoroutineScope
+    ) : Call<Any> {
+        override fun clone(): Call<Any> {
+            return CallEnqueueDelegate(originCall.clone(), coroutineScope)
+        }
+
+        override fun execute(): Response<Any> {
+            return runBlocking(coroutineScope.coroutineContext) {
+                val apiResponse = ApiResponse.of(originCall.execute())
+                Timber.d("Execute ApiResponse $apiResponse")
+                Response.success(apiResponse)
+            }
+        }
+
+        override fun enqueue(callback: Callback<Any>) {
+            coroutineScope.launch {
+                val apiResponse = ApiResponse.of(originCall.awaitResponse())
+                Timber.d("Enqueue ApiResponse $apiResponse")
+                callback.onResponse(this@CallEnqueueDelegate, Response.success(apiResponse))
             }
         }
 
         override fun isExecuted(): Boolean {
             return originCall.isExecuted
+        }
+
+        override fun cancel() {
+            return originCall.cancel()
         }
 
         override fun isCanceled(): Boolean {
@@ -97,26 +116,80 @@ class CoroutineErrorHandlingCallAdapter(
         override fun timeout(): Timeout {
             return originCall.timeout()
         }
-
-        override fun execute(): Response<ApiResponse<Any>> {
-            return runBlocking(coroutineScope.coroutineContext) {
-                try {
-                    val response = originCall.execute()
-                    val body = response.body()
-                    Timber.d("execute ${response.code()}")
-                    if (response.isSuccessful && body != null) {
-                        Response.success(ApiResponse.Success(body))
-                    } else {
-                        Response.success(ApiResponse.Fail(response))
-                    }
-                } catch (ex: Throwable) {
-                    Response.success(ApiResponse.Error(ex))
-                }
-            }
-        }
-
-        override fun cancel() {
-            originCall.cancel()
-        }
     }
+
+//    private class CallExecuteCoroutinesDelegate(
+//        private val originCall: Call<Any>,
+//        private val coroutineScope: CoroutineScope
+//    ) : Call<ApiResponse<Any>> {
+//        override fun clone(): Call<ApiResponse<Any>> {
+//            return CallExecuteCoroutinesDelegate(originCall.clone(), coroutineScope)
+//        }
+//
+//        override fun enqueue(callback: Callback<ApiResponse<Any>>) {
+//            coroutineScope.launch {
+//                try {
+//                    val response = originCall.awaitResponse()
+//                    val body = response.body()
+//                    if (body != null) {
+//                        Timber.d("enqueue SUCCESS $body")
+//                        callback.onResponse(
+//                            this@CallExecuteCoroutinesDelegate,
+//                            Response.success(ApiResponse.Success(body))
+//                        )
+//                    } else {
+//                        Timber.d("enqueue FAIL $response")
+//                        callback.onResponse(
+//                            this@CallExecuteCoroutinesDelegate,
+//                            Response.success(ApiResponse.Fail(response))
+//                        )
+//                    }
+//
+//                } catch (ex: Exception) {
+//                    Timber.d("enqueue ERROR $ex")
+//                    callback.onResponse(
+//                        this@CallExecuteCoroutinesDelegate,
+//                        Response.success(ApiResponse.Error(ex))
+//                    )
+//                }
+//            }
+//        }
+//
+//        override fun isExecuted(): Boolean {
+//            return originCall.isExecuted
+//        }
+//
+//        override fun isCanceled(): Boolean {
+//            return originCall.isCanceled
+//        }
+//
+//        override fun request(): Request {
+//            return originCall.request()
+//        }
+//
+//        override fun timeout(): Timeout {
+//            return originCall.timeout()
+//        }
+//
+//        override fun execute(): Response<ApiResponse<Any>> {
+//            return runBlocking(coroutineScope.coroutineContext) {
+//                try {
+//                    val response = originCall.execute()
+//                    val body = response.body()
+//                    Timber.d("execute ${response.code()}")
+//                    if (response.isSuccessful && body != null) {
+//                        Response.success(ApiResponse.Success(body))
+//                    } else {
+//                        Response.success(ApiResponse.Fail(response))
+//                    }
+//                } catch (ex: Throwable) {
+//                    Response.success(ApiResponse.Error(ex))
+//                }
+//            }
+//        }
+//
+//        override fun cancel() {
+//            originCall.cancel()
+//        }
+//    }
 }
