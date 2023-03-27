cc62a8ad156f4df432456640490d82808fa87108
sieunju <j.sieun73@gmail.com>
2023-03-27 23:10:28 +0900

    fix: 잠깐 refactor_network PR 했는데 깃허브 500 이슈로 머지가 잘못된 부분 재 반여

--- android/network/src/main/java/com/hmju/network/qualifiers/ApiHttpClient.kt
+++
@@ -1,7 +0,0 @@
-package com.hmju.network.qualifiers
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION)
-annotation class ApiHttpClient
