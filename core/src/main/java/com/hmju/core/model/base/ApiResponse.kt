535fc77aee82527cf161c4f54ef4343c877681df
sieunju <j.sieun73@gmail.com>
2023-11-23 22:20:16 +0900

    refactor: Network V2 리펙토링 - ApiResponse 모델 개선 - JSendException 안에 케이스 별로 처리

--- core/src/main/java/com/hmju/core/model/base/ApiResponse.kt
+++ core/src/main/java/com/hmju/core/model/base/ApiResponse.kt
@@ -7,33 +7,10 @@ package com.hmju.core.model.base
  */
 sealed class ApiResponse<out T> {
 
-    enum class FailType {
-        NETWORK, HTTP, UN_KNOWN
-    }
-
     /**
      * Network Success Response Data
      */
     data class Success<out T>(val data: T) : ApiResponse<T>()
 
-    data class Fail(
-        val err: Throwable
-    ) : ApiResponse<Nothing>()
-
-//    /**
-//     * Network Fail Data Model
-//     * @see FailType.HTTP HTTP Error
-//     * @see FailType.NETWORK Network Config Error
-//     * @see FailType.UN_KNOWN UN KNOWN
-//     *
-//     * @param type FailType
-//     * @param msg Error Message
-//     * @param errBody when [FailType.HTTP] Error Body
-//     */
-//    data class Fail(
-//        val type: FailType,
-//        val msg: CharSequence,
-//        val err: Throwable? = null,
-//        val errBody: ResponseBody? = null
-//    ) : ApiResponse<Nothing>()
+    data class Fail(val err: Throwable) : ApiResponse<Nothing>()
 }
