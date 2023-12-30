736d608700f0e65b65e6f149324c61a736412c47
sieunju <j.sieun73@gmail.com>
2023-12-30 09:57:37 +0900

    feat: Compose ViewModel 추가

--- features/compose-ui/src/main/java/com/feature/compose_ui/ComposeUiActivityViewModel.kt
+++ features/compose-ui/src/main/java/com/feature/compose_ui/ComposeUiActivityViewModel.kt
@@ -1,7 +1,17 @@
 package com.feature.compose_ui
 
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewModelScope
 import com.hmju.core.ui.base.ActivityViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 /**
@@ -13,4 +23,23 @@ import javax.inject.Inject
 class ComposeUiActivityViewModel @Inject constructor(
 
 ) : ActivityViewModel() {
+    sealed interface UiState {
+        object Loading : UiState
+
+        data class Success(
+            val value: String
+        ) : UiState
+
+        data class Error(val err: String) : UiState
+    }
+
+    private val _uiState: MutableStateFlow<UiState> by lazy { MutableStateFlow(UiState.Loading) }
+
+    val message: StateFlow<String> = _uiState
+        .map { if (it is UiState.Success) it.value else "" }
+        .stateIn(viewModelScope, SharingStarted.Eagerly, initialValue = "33")
+
+    override fun onDirectCreate() {
+        super.onDirectCreate()
+    }
 }
