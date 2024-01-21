a1008729d2e8c0c5d445b3d67446c6b376fcd429
sieunju <j.sieun73@gmail.com>
2024-01-21 13:55:08 +0900

    test: TilTheme ColorStyle 예제 코드 추가

--- features/compose-ui/src/main/java/com/feature/compose_ui/TilThemeExample.kt
+++ features/compose-ui/src/main/java/com/feature/compose_ui/TilThemeExample.kt
@@ -1,12 +1,17 @@
 package com.feature.compose_ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +37,7 @@ class TilThemeExample {
 
     @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
     @Composable
-    fun TextStyleExample() {
+    private fun TextStyleExample() {
         Column(
             modifier = Modifier.wrapContentWidth()
         ) {
@@ -66,6 +71,45 @@ class TilThemeExample {
                 TextStyleExampleItem("H5_M", TilTheme.text.h5_M)
                 TextStyleExampleItem("H5_B", TilTheme.text.h5_B)
             }
+        }
+    }
+
+    @Composable
+    private fun ColorStyleExampleItem(
+        text: String,
+        color: Color
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = color)
+                .width(300.dp)
+        ) {
+            Text(
+                text = text,
+                style = TilTheme.text.h4_M,
+                color = if (text == "white") TilTheme.color.black else TilTheme.color.white
+            )
+        }
+    }
+
+    @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+    @Composable
+    private fun ColorStyleExample() {
+        Column(
+            modifier = Modifier.wrapContentWidth()
+        ) {
+            ColorStyleExampleItem("white", TilTheme.color.white)
+            ColorStyleExampleItem("black", TilTheme.color.black)
+            ColorStyleExampleItem("blue", TilTheme.color.blue)
+            ColorStyleExampleItem("red", TilTheme.color.red)
+            ColorStyleExampleItem("purple", TilTheme.color.purple)
+            ColorStyleExampleItem("yellow", TilTheme.color.yellow)
+            ColorStyleExampleItem("gray1", TilTheme.color.gray1)
+            ColorStyleExampleItem("gray2", TilTheme.color.gray2)
+            ColorStyleExampleItem("gray3", TilTheme.color.gray3)
+            ColorStyleExampleItem("gray3Light", TilTheme.color.gray3Light)
+            ColorStyleExampleItem("gray4", TilTheme.color.gray4)
+            ColorStyleExampleItem("gray5", TilTheme.color.gray5)
         }
     }
 }
