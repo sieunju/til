78965545f0ff6a97eba0656df559555c114da9b4
sieunju <j.sieun73@gmail.com>
2024-02-08 22:20:18 +0900

    chore: GlideBindingAdapter Fade 애니메이션 추가

--- core/src/main/java/com/hmju/core/ui/binding/GlideBindingAdapter.kt
+++ core/src/main/java/com/hmju/core/ui/binding/GlideBindingAdapter.kt
@@ -4,6 +4,8 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 
 /**
  * Description : ImageLoader BindingAdapter
@@ -11,6 +13,15 @@ import com.bumptech.glide.Glide
  * Created by juhongmin on 2022/07/23
  */
 object GlideBindingAdapter {
+
+    private val crossFadeFactory: DrawableCrossFadeFactory by lazy {
+        DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
+    }
+
+    private val crossFadeTransition: DrawableTransitionOptions by lazy {
+        DrawableTransitionOptions.withCrossFade(crossFadeFactory)
+    }
+
     @JvmStatic
     @BindingAdapter("imgPath")
     fun setLoadImageUrl(
@@ -24,6 +35,7 @@ object GlideBindingAdapter {
 
         Glide.with(img.context)
             .load(url)
+            .transition(crossFadeTransition)
             .into(img)
     }
 }
