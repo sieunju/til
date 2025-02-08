4665ce275f147323e41faa48cb4c421af4cd0f42
sieunju <j.sieun73@gmail.com>
2025-02-08 21:43:22 +0900

    chore: google playstore 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.*
 
 plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
 
 buildscript {
